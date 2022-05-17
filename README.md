8dd892f2e13be89d79591b019f445eeb6bf8e61b
colin brindle <hcolinbrindle@gmail.com>
2022-05-17 15:00:21 -0400

    Finalized day planner script and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# day-planner
+# Day Planner
+
+## **Description**
+
+A simple calendar application to plan out your daily tasks.
+
+This application utilizes local storage and several third-party APIs including: Bootstrap, Moment.js, JQuery, and Google Fonts.
+
+## **Site Screenshots**
+
+![Screenshot of site](./imgs/site.png)
+
+![Screenshot of local storage](./imgs/localstorage.png)
--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 console.log(`Scripted linked`);
+
+// Using JQuery ready method to run functions on site load
+$(document).ready(function () {
+  // Function to display and update current date and time
+  const currentDateTime = function () {
+    $("#current-day").text(moment().format("dddd, MMMM Do"));
+    $("#current-time").text(moment().format("h:mm A"));
+  };
+
+  // Running currentDateTime on load
+  currentDateTime();
+
+  // Interval to update date and time every 30 seconds
+  setInterval(currentDateTime, 30000);
+
+  // Function to update row colors
+  const rowColor = function () {
+    let currentTime = moment().hours();
+    console.log(currentTime);
+
+    // For each loop to check and update row colors for all time-block elements
+    $(".time-block").each(function () {
+      let parsedRowTime = $(this).attr("id").split("-");
+      let rowTime = parseInt(parsedRowTime[1]);
+      console.log(parsedRowTime);
+      console.log(rowTime);
+
+      if (rowTime < currentTime) {
+        $(this).addClass("past");
+      } else if (rowTime > currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+    });
+  };
+
+  // Updating row colors on load
+  rowColor();
+  // Running rowColor function every 6 minutes
+  setInterval(rowColor, 360000);
+
+  // Function to save notes
+  const saveNotes = function () {
+    console.log("Button click registered");
+
+    let hourId = $(this).parent().attr("id");
+    let hourNotes = $(this).siblings(".text-box").val();
+
+    localStorage.setItem(hourId, hourNotes);
+  };
+
+  //   Event listener for save button click
+  $(".saveBtn").on("click", saveNotes);
+
+  // For each loop to load locally stored data
+  $(".time-block").each(function () {
+    let hourId = $(this).attr("id");
+
+    $(this).find(".text-box").val(localStorage.getItem(hourId));
+  });
+});
